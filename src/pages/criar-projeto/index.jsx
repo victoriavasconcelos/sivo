eb3899d01debd517983cc16607dd7e74848840d6
Fernando Angelim Aragão <fernandoangelima@gmail.com>
2023-09-18 20:23:05 -0300

    Alteração da tela criar-projeto para MUI

--- src/pages/criar-projeto/index.jsx
+++ src/pages/criar-projeto/index.jsx
@@ -1,49 +1,104 @@
+import { Button, Grid, TextField } from "@mui/material";
 import { blue } from "@mui/material/colors";
+import { useState } from "react";
 
 export default function CriarProjeto (){
+
+    const [nome, setNome] = useState("")
+    const [disciplina, setDisciplina] = useState("")
+    const [descricao, setDescricao] = useState("")
+    const [vagas, setVagas] = useState("")
+    const [tempo, setTempo] = useState("")
+    const [cronograma, setCronograma] = useState("")
+    
     return (
-        <><head>
-            <title>Criar Projeto</title>
-            <style>
-
-
-            </style>
-        </head>
-        <body>
-            <section>
-                <form>
-                    <div>
-                        <label>Nome do Projeto</label>
-                        <input type="text" name="Nome do Projeto" required></input>
-                    </div>
-                    <div>
-                        <label>Disciplinas</label>
-                        <input type="text" name="disciplinas" required />
-                    </div>
-                    <div id="conteiner-a">
-                        <label>Descrição</label>
-                        <textarea rows="8" name="msg" required></textarea>
-                    </div>
-                    <div>
-                        <label>Vagas Ofertadas</label>
-                        <input type="number" max="10" min="0" />
-                    </div>
-                    <div>
-                        <label>Tempo de duração</label>
-                        <input type="month" />
-                    </div>
-                    <div id="conteiner-b">
-                        <label>Cronograma</label>
-                        <textarea rows="15" required></textarea>
-                    </div>
-                    <div>
-                        <input type="submit" value="Enviar" />
-                    </div>
-                </form>
-            </section>
-        </body>
-        </>
-
+        <section>
+            <form>
+                <Grid container spacing={2}>
+                    <Grid container item xs={12} spacing={2}>
+                        <Grid item xs={6}>
+                            <TextField
+                                label="Nome do Projeto"
+                                required
+                                fullWidth
+                                value={nome}
+                                onChange={e => setNome(e.target.value)}
+                                multiline
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                label="Disciplinas"
+                                required
+                                fullWidth
+                                value={disciplina}
+                                onChange={e => setDisciplina(e.target.value)}
+                                multiline
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid item container spacing={2}>
+                        <Grid item xs={6}>
+                            <TextField
+                                label="Descrição"
+                                required
+                                fullWidth
+                                value={descricao}
+                                onChange={e => setDescricao(e.target.value)}
+                                multiline
+                                rows={5}
+                            />
+                        </Grid>
+                        <Grid item xs={6} container direction="column">
+                            <Grid item>
+                                <TextField
+                                    label="Vagas Ofertadas"
+                                    required
+                                    fullWidth
+                                    type="number"
+                                    value={vagas}
+                                    onChange={e => setVagas(e.target.value)}
+                                    multiline
+                                />
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    label="Tempo de Duração"
+                                    required
+                                    value={tempo}
+                                    onChange={e => setTempo(e.target.value)}
+                                    multiline
+                                    fullWidth
+                                />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="Cronograma"
+                            required
+                            fullWidth
+                            value={cronograma}
+                            onChange={e => setCronograma(e.target.value)}
+                            multiline
+                            rows={10}
+                        />
+                    </Grid>
+                    <Grid container item xs={12} justifyContent="center">
+                        <Grid item xs={2}>
+                            <Button
+                                variant="contained"
+                                type="submit"
+                                fullWidth
+                                
+                            >
+                                Concluir
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </form>
+        </section>
     )
 }
 
